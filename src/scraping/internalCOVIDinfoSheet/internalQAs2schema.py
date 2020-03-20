364acb5db3e685fdc270e2ff20d3eb0cad47f44a
Adam Poliak <azpoliak@gmail.com>
2020-03-20 18:30:58 -0400

    scraping: updated internalCovid based on new manually added QAs

--- src/scraping/internalCOVIDinfoSheet/internalQAs2schema.py
+++ src/scraping/internalCOVIDinfoSheet/internalQAs2schema.py
@@ -7,31 +7,36 @@ from covid_scraping import test_jsonlines
 
 def to_schema(row):
   # ['Question', 'Answer', 'Need to update (Y/N)', 'Tags', 'Author']
+  # Question  Answer  Need to update (Y/N)  Tags  Source  Annotator/Author
   data = {
-  "sourceUrl": "Internal COVID19infosheet",
-  "sourceName": "JHU Public Health",
-  "dateScraped": time.time(),
-  "sourceDate": float(1584717464),
-  "lastUpdateTime": float(1584717464),
-  "needUpdate": True,
-  "containsURLs": True, #need to make this programmitic
-  "typeOfInfo": "QA",
-  "isAnnotated": True,
-  "responseAuthority": "Shivani Pandya or Smisha Agrawal",
-  "questionUUID": str(uuid.uuid1()),
-  "answerUUID": str(uuid.uuid1()),
-  "exampleUUID": str(uuid.uuid1()),
-  "questionText": row['Question'],
-  "answerText": row['Answer'] if not pd.isna(row['Answer']) else "",
-  "hasAnswer": not pd.isna(row['Answer']),
-  "targetEducationLevel": "NA",
-  "topic": row['Tags'] if not pd.isna(row['Tags']) else "",
-  "extraData": {}}
+    "sourceUrl": "Internal COVID19infosheet",
+    "sourceName": "JHU Public Health" if pd.isna(row['Source']) else "JHU Public Health " + row['Source'],
+    "dateScraped": time.time(),
+    "sourceDate": float(1584717464),
+    "lastUpdateTime": float(1584717464),
+    "needUpdate": True,
+    "containsURLs": True, #need to make this programmitic
+    "typeOfInfo": "QA",
+    "isAnnotated": True,
+    "responseAuthority": "Shivani Pandya or Smisha Agrawal",
+    "questionUUID": str(uuid.uuid1()),
+    "answerUUID": str(uuid.uuid1()),
+    "exampleUUID": str(uuid.uuid1()),
+    "questionText": row['Question'],
+    "answerText": row['Answer'] if not pd.isna(row['Answer']) else "",
+    "hasAnswer": not pd.isna(row['Answer']),
+    "targetEducationLevel": "NA",
+    "topic": row['Tags'] if not pd.isna(row['Tags']) else "",
+    "extraData": {
+      "author": row['Annotator/Author']
+    }
+  }
   return data
 
 def main():
   df = pd.read_csv("COVID19infosheet - Info.tsv", sep="\t")
   df = df[df['Question'].map(lambda x: not pd.isna(x))]
+  df = df[df['Question'] != 'Questions from Smisha']
   df['json'] = df.apply(to_schema, axis=1)
   with jsonlines.open('../../../data/scraping/interalCOVIDinfosheet_v0.1.jsonl', 'w') as writer:
     writer.write_all(df['json'])
