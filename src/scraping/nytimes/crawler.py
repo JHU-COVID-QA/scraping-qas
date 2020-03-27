7f7efb81c8351922564901ffd89b39a717007cda
Milind Agarwal <36268591+magarw@users.noreply.github.com>
2020-03-27 11:04:10 -0400

    Added the header information as well

--- src/scraping/nytimes/crawler.py
+++ src/scraping/nytimes/crawler.py
@@ -1,3 +1,20 @@
+# Copyright (c) Johns Hopkins University and its affiliates.
+# This source code is licensed under the Apache 2 license found in the
+# LICENSE file in the root directory of this source tree.
+"""
+NYTimes crawler 
+Expected page to crawl is 
+https://www.nytimes.com/interactive/2020/world/coronavirus-tips-advice.html
+"""
+__author__ = "Kaushik Srinivasan and Milind Agarwal"
+__copyright__ = "Copyright 2020, Johns Hopkins University"
+__credits__ = ["Kaushik Srinivasan", "Adam Poliak"]
+__license__ = "Apache 2.0"
+__version__ = "0.1"
+__maintainer__ = "JHU-COVID-QA"
+__email__ = "covidqa@jhu.edu"
+__status__ = "Development"
+
 import datetime, time
 import pprint
 from urllib import request, response, error, parse
