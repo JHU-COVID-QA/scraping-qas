48b34001323fe8f1f4e6f01a6f150e32b0b2025a
Max Fleming <mjfleming99@gmail.com>
2020-04-21 11:21:30 -0700

    Adding unit tests for uuid preservation.

--- src/scraping/tests/test_conversion.py
+++ src/scraping/tests/test_conversion.py
@@ -38,15 +38,19 @@ class TestConversion(unittest.TestCase):
         test_jsonlines('./schema_v0.1/test_v0.1.jsonl', version='v0.1')
         with jsonlines.open('./schema_v0.1/test_v0.1.jsonl', 'r') as reader:
             line = reader.read()
-            self.assertEqual(line['questionText'], 'What is COVID-19?')
-            self.assertEqual(line['answerText'], 'Coronaviruses are a large family of viruses.')
+            self.assertEqual(line['questionText'], 'What is COVID-19 ?')
+            self.assertEqual(
+                line['answerText'],
+                'Coronaviruses are a large family of viruses .')
 
     def test_schema_v02(self):
         test_jsonlines('./schema_v0.2/test_v0.2.jsonl', version='v0.2')
         with jsonlines.open('./schema_v0.2/test_v0.2.jsonl', 'r') as reader:
             line = reader.read()
-            self.assertEqual(line['questionText'], 'What is COVID-19?')
-            self.assertEqual(line['answerText'], 'Coronaviruses are a large family of viruses.')
+            self.assertEqual(line['questionText'], 'What is COVID-19 ?')
+            self.assertEqual(
+                line['answerText'],
+                'Coronaviruses are a large family of viruses .')
 
     def test_key_exception(self):
         with self.assertRaises(KeyError) as ke:
@@ -60,48 +64,71 @@ class TestConversion(unittest.TestCase):
         with self.assertRaises(ValueError) as ve:
             converter = Conversion('test', '.')
             converter.addExample({
-            'sourceUrl': ['example.com'],
-            "language": 'en',
+                'sourceUrl': ['example.com'],
+                "language": 'en',
             })
-
 
     def test_blank_question_exception(self):
         with self.assertRaises(ValueError) as e:
             converter = Conversion('test', '.')
             converter.addExample({
-            'sourceUrl': 'example.com',
-            'sourceName': "example",
-            "sourceDate": 1585777414.515401,
-            "lastUpdateTime": 1585777414.515401,
-            "needUpdate": True,
-            "typeOfInfo": "QA",
-            "isAnnotated": False,
-            "responseAuthority": "",
-            "question": '               ',
-            "answer": '<a href="example.com/dir1">What is COVID-19?</a>',
-            "hasAnswer": True,
-            "targetEducationLevel": "NA",
-            "topic": ['topic1', 'topic2'],
-            "extraData": {'hello': 'goodbye'},
-            "targetLocation": "US",
-            "language": 'en',
-        })
-
+                'sourceUrl': 'example.com',
+                'sourceName': "example",
+                "sourceDate": 1585777414.515401,
+                "lastUpdateTime": 1585777414.515401,
+                "needUpdate": True,
+                "typeOfInfo": "QA",
+                "isAnnotated": False,
+                "responseAuthority": "",
+                "question": '               ',
+                "answer": '<a href="example.com/dir1">What is COVID-19?</a>',
+                "hasAnswer": True,
+                "targetEducationLevel": "NA",
+                "topic": ['topic1', 'topic2'],
+                "extraData": {'hello': 'goodbye'},
+                "targetLocation": "US",
+                "language": 'en',
+            })
 
     def test_blank_answer_exception(self):
         with self.assertRaises(ValueError) as e:
             converter = Conversion('test', '.')
             converter.addExample({
-            'sourceUrl': 'example.com',
-            'sourceName': "example",
+                'sourceUrl': 'example.com',
+                'sourceName': "example",
+                "sourceDate": 1585777414.515401,
+                "lastUpdateTime": 1585777414.515401,
+                "needUpdate": True,
+                "typeOfInfo": "QA",
+                "isAnnotated": False,
+                "responseAuthority": "",
+                "question": '<a href="example.com/dir1">What is COVID-19?</a>',
+                "answer": '\n    \n',
+                "hasAnswer": True,
+                "targetEducationLevel": "NA",
+                "topic": ['topic1', 'topic2'],
+                "extraData": {'hello': 'goodbye'},
+                "targetLocation": "US",
+                "language": 'en',
+            })
+
+    def test_uuid_preservation_no_change(self):
+        subprocess.run(
+            ['touch', './schema_v0.1/test_uuid_preservation_v0.1.jsonl'])
+        subprocess.run(
+            ['touch', './schema_v0.2/test_uuid_preservation_v0.2.jsonl'])
+        converter = Conversion('test_uuid_preservation', '.')
+        converter.addExample({
+            'sourceUrl': 'uuid.com',
+            'sourceName': "uuid",
             "sourceDate": 1585777414.515401,
             "lastUpdateTime": 1585777414.515401,
             "needUpdate": True,
             "typeOfInfo": "QA",
             "isAnnotated": False,
             "responseAuthority": "",
-            "question": '<a href="example.com/dir1">What is COVID-19?</a>',
-            "answer": '\n    \n',
+            "question": 'Hello, this is the example question?',
+            "answer": 'Hello this is the example responce',
             "hasAnswer": True,
             "targetEducationLevel": "NA",
             "topic": ['topic1', 'topic2'],
@@ -109,7 +136,102 @@ class TestConversion(unittest.TestCase):
             "targetLocation": "US",
             "language": 'en',
         })
+        converter.write()
+        with jsonlines.open('./schema_v0.2/test_uuid_preservation_v0.2.jsonl') as reader:
+            line = reader.read()
+            q_uuid = line['questionUUID']
+            a_uuid = line['answerUUID']
+        converter = Conversion('test_uuid_preservation', '.')
+        converter.addExample({
+            'sourceUrl': 'uuid.com',
+            'sourceName': "uuid",
+            "sourceDate": 1585777414.515401,
+            "lastUpdateTime": 1585777414.515401,
+            "needUpdate": True,
+            "typeOfInfo": "QA",
+            "isAnnotated": False,
+            "responseAuthority": "",
+            "question": 'Hello, this is the example question?',
+            "answer": 'Hello this is the example responce',
+            "hasAnswer": True,
+            "targetEducationLevel": "NA",
+            "topic": ['topic1', 'topic2'],
+            "extraData": {'hello': 'goodbye'},
+            "targetLocation": "US",
+            "language": 'en',
+        })
+        converter.write()
+        with jsonlines.open('./schema_v0.2/test_uuid_preservation_v0.2.jsonl') as reader:
+            line = reader.read()
+            new_q_uuid = line['questionUUID']
+            new_a_uuid = line['answerUUID']
+        self.assertEqual(q_uuid, new_q_uuid)
+        self.assertEqual(a_uuid, new_a_uuid)
+        subprocess.run(
+            ['rm', './schema_v0.1/test_uuid_preservation_v0.1.jsonl'])
+        subprocess.run(
+            ['rm', './schema_v0.2/test_uuid_preservation_v0.2.jsonl'])
 
+    def test_uuid_preservation_new_responce(self):
+        subprocess.run(
+            ['touch', './schema_v0.1/test_uuid_preservation_v0.1.jsonl'])
+        subprocess.run(
+            ['touch', './schema_v0.2/test_uuid_preservation_v0.2.jsonl'])
+
+        converter = Conversion('test_uuid_preservation', '.')
+        converter.addExample({
+            'sourceUrl': 'uuid.com',
+            'sourceName': "uuid",
+            "sourceDate": 1585777414.515401,
+            "lastUpdateTime": 1585777414.515401,
+            "needUpdate": True,
+            "typeOfInfo": "QA",
+            "isAnnotated": False,
+            "responseAuthority": "",
+            "question": 'Hello, this is the example question?',
+            "answer": 'Hello this is the example responce',
+            "hasAnswer": True,
+            "targetEducationLevel": "NA",
+            "topic": ['topic1', 'topic2'],
+            "extraData": {'hello': 'goodbye'},
+            "targetLocation": "US",
+            "language": 'en',
+        })
+        converter.write()
+        with jsonlines.open('./schema_v0.2/test_uuid_preservation_v0.2.jsonl') as reader:
+            line = reader.read()
+            q_uuid = line['questionUUID']
+            a_uuid = line['answerUUID']
+        converter = Conversion('test_uuid_preservation', '.')
+        converter.addExample({
+            'sourceUrl': 'uuid.com',
+            'sourceName': "uuid",
+            "sourceDate": 1585777414.515401,
+            "lastUpdateTime": 1585777414.515401,
+            "needUpdate": True,
+            "typeOfInfo": "QA",
+            "isAnnotated": False,
+            "responseAuthority": "",
+            "question": 'Hello, this is the example question?',
+            "answer": 'This completely different this should cause a new UUID',
+            "hasAnswer": True,
+            "targetEducationLevel": "NA",
+            "topic": ['topic1', 'topic2'],
+            "extraData": {'hello': 'goodbye'},
+            "targetLocation": "US",
+            "language": 'en',
+        })
+        converter.write()
+        with jsonlines.open('./schema_v0.2/test_uuid_preservation_v0.2.jsonl') as reader:
+            line = reader.read()
+            new_q_uuid = line['questionUUID']
+            new_a_uuid = line['answerUUID']
+        self.assertEqual(q_uuid, new_q_uuid)
+        self.assertNotEqual(a_uuid, new_a_uuid)
+        subprocess.run(
+            ['rm', './schema_v0.1/test_uuid_preservation_v0.1.jsonl'])
+        subprocess.run(
+            ['rm', './schema_v0.2/test_uuid_preservation_v0.2.jsonl'])
 
 
 if __name__ == '__main__':
