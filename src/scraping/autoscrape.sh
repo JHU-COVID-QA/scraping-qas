b8241b7adf0fb00222784f0b3ec2de35e1c64cf3
Ubuntu <ubuntu@dev.covid-19-bot.org>
2020-04-28 16:04:47 +0000

    Updated autoscrape.sh to meet comments

--- src/scraping/autoscrape.sh
+++ src/scraping/autoscrape.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 #This script reruns all of the scrapers.
-#It is dependant on conda and having a github deploy key configured
+#It is dependant on anaconda3 in the home directory and having a github deploy key configured
 #It also requires that the public spreadsheet is avalable at the url in the script
 
-base_dir=$(echo "/home/ubuntu/autoscraper")
-log_file=$(date +"/home/ubuntu/autoscraper/src/scraping/autoscrape_logs/autoscrape-%b-%d-%H.log")
-cd $base_dir/src/scraping
+base_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+log_file=$(date +"$base_dir/autoscrape_logs/autoscrape-%b-%d-%H.log")
 
 branch_name=$(date +"autoscrape-test-%b-%d-%H")
 
@@ -15,7 +14,7 @@ git checkout master
 git pull
 git checkout -b $branch_name &>> $log_file
 
-eval "$(/home/ubuntu/anaconda3/bin/conda shell.bash hook)" &>> $log_file
+eval "$($HOME/anaconda3/bin/conda shell.bash hook)" &>> $log_file
 CONDA_BASE=$(conda info --base)
 source $CONDA_BASE/etc/profile.d/conda.sh &>> $log_file
 conda env update  --file environment.yml  --prune &>> $log_file
@@ -23,18 +22,18 @@ conda activate crawler &>> $log_file
 python setup.py install &>> $log_file
 
 
-cd $base_dir/src/scraping/scrapers
+cd $base_dir/scrapers
 wget --no-check-certificate -O COVID19infosheet\ -\ Info.tsv  "https://docs.google.com/spreadsheets/d/1Drmwo62V4MvB1X6eTwi1L-f3EYq09oocQ2Jvo-XR1TQ/export?gid=0&format=tsv" &>> $log_file
 python scrape_all.py &>> $log_file
 python deepsetAI_scraper.py &>> $log_file 
 python make_public.py --path $base_dir/data/scraping/schema_v0.2/
 
-cd $base_dir/data/scraping
+cd $base_dir/../../data/scraping
 git add schema_v0.1/* schema_v0.2/*  &>> $log_file
 git commit -m $(date +"autoscrape-%b-%d-%H") &>> $log_file
 git push --set-upstream origin $branch_name &>> $log_file
 
-cd $base_dir/src/scraping
+cd $base_dir
 python log_to_json.py $log_file
 curl -X POST -H 'Content-type: application/json' --data @tmp.json  https://hooks.slack.com/services/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
 rm tmp.json
